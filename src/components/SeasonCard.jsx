ac6851a5d3f5c426d74bd3c65a14ac7f71c7470b
Alex Boisseau <alex.boisseau79@gmail.com>
2022-02-11 06:31:16 +0100

    Calculate days since the start of the season in SeasonCard component

--- src/components/SeasonCard.jsx
+++ src/components/SeasonCard.jsx
@@ -1,10 +1,21 @@
+import { formatDistanceToNowStrict } from 'date-fns';
+
 export default function SeasonCard({ onClick, season }) {
+  const sinceDays = formatDistanceToNowStrict(
+    new Date(
+      new Date().getFullYear() + (season.name === 'Winter' ? -1 : 0),
+      season.beginAt.month - 1,
+      season.beginAt.day
+    ),
+    { unit: 'day', addSuffix: true }
+  );
+
   return (
     <div className="shadow-black-500/50 rounded bg-gray-100 py-24 px-16 text-center shadow-lg">
       <h1 className="text-4xl font-bold">
         {season.name} {season.icon}
       </h1>
-      <p className="text-gray-500">Since 21 days</p>
+      <p className="text-gray-500">Since {sinceDays}</p>
       <button
         className="mt-5 rounded bg-red-500 p-3 text-white shadow shadow-md hover:bg-red-600"
         onClick={() => onClick()}
